ff46d00cb7a593a6b349d8a87905cd7c837768f8
Hanson Yang <6323527@edu.vaniercollege.qc.ca>
2024-11-19 09:22:29 -0500

    Finished exam

--- starter-final/CannonBall.java
+++ starter-final/CannonBall.java
@@ -1,4 +1,3 @@
-// WARNING: This file is auto-generated and any changes to it will be overwritten
 import lang.stride.*;
 import java.util.*;
 import greenfoot.*;
@@ -8,11 +7,35 @@ import greenfoot.*;
  */
 public class CannonBall extends Actor
 {
-
+    private int speed;
+    public CannonBall(int angle, int speed){
+        turn(angle);
+        this.speed = speed;
+        
+    }
+    
     /**
      * Act - do whatever the CannonBall wants to do. This method is called whenever the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act()
     {
+        move(speed);
+        killBugAndTransition();
+        destroySelf();
+    }
+    
+    public void destroySelf(){
+        if (isAtEdge()){
+            getWorld().removeObject(this);
+        }
+    }
+    
+    public void killBugAndTransition(){
+        if (isTouching(Ladybug.class)){
+            World world = getWorld();
+            World gameOverWorld =  new GameOverWorld();
+            gameOverWorld.started();
+            Greenfoot.setWorld(gameOverWorld);
+        }
     }
 }
